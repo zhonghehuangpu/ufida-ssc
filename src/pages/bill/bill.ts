072832a4b0e98589167f1d18f25ab0748d7b7d77
zhaojunming <zhao>
2016-10-12 22:36:26 +0800

    delete

--- src/pages/bill/bill.ts
+++ src/pages/bill/bill.ts
@@ -1,22 +1,38 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
 
 /*
-  Generated class for the Bill page.
+ Generated class for the Bill page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
+ See http://ionicframework.com/docs/v2/components/#navigation for more info on
+ Ionic pages and navigation.
+ */
 @Component({
-  selector: 'page-bill',
-  templateUrl: 'bill.html'
+    selector: 'page-bill',
+    templateUrl: 'bill.html'
 })
 export class Bill {
 
-  constructor(public navCtrl: NavController) {}
 
-  ionViewDidLoad() {
-    console.log('Hello Bill Page');
-  }
+    filterDate: string;
+
+    constructor(public navCtrl: NavController) {
+    }
+
+    ionViewDidLoad() {
+        console.log('Hello Bill Page');
+    }
+
+    openDataTime(datetime: Element) {
+
+        console.log(datetime);
+
+        var e = document.createEvent('MouseEvent');
+        e.initEvent('click', false, false);
+
+        datetime._elementRef.nativeElement.click()
+        datetime._elementRef.nativeElement.dispatchEvent(e);
+
+    }
 
 }

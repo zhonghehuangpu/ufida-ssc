99828bee4a7ad4329a9e4cee3bf84b423f00852f
zhaojunming <zhao>
2016-10-12 16:50:04 +0800

    delete

---
+++ src/pages/menuOptions/option-introdcue/option-introdcue.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the OptionIntrodcue page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-option-introdcue',
+  templateUrl: 'option-introdcue.html'
+})
+export class OptionIntrodcue {
+
+  constructor(public navCtrl: NavController) {}
+
+  ionViewDidLoad() {
+    console.log('Hello OptionIntrodcue Page');
+  }
+
+}
